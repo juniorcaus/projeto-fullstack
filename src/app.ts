a13752ad9fec21c4af0245372fbebed6210d00f5
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-01-10 22:53:43 -0300

    Criando UserController

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-import express from 'express';
+import express, { Router } from 'express';
+
 import cors from 'cors';
 import mongoose from 'mongoose';
+import Controller from './controllers/controller';
 
 class App {
   public app: express.Application;
 
-  public constructor() {
+  public constructor(controllers: Controller[]) {
     this.app = express();
     this.app.use(cors());
 
     this.initMongoose();
     this.connectDatabase();
+    this.initExpressJson();
+    this.initControllers(controllers);
   }
 
   private initMongoose(): void {
@@ -23,6 +27,16 @@ class App {
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
+    });
+  }
+
+  private initExpressJson(): void {
+    this.app.use(express.json());
+  }
+
+  private initControllers(controllers: Controller[]):void {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router);
     });
   }
 
---
+++ src/controllers/UserController.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import Controller from './controller';
+import User from '../schemas/User';
+
+class UserController extends Controller {
+  constructor() {
+    super('user');
+  }
+
+  protected initRoutes(): void {
+    this.router.get(this.path, this.list);
+  }
+
+  private async list(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const users = await User.find();
+
+    return res.send(users);
+  }
+}
+
+export default UserController;
